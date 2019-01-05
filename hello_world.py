7bf1e9a746bfe3871856efb9acc328052d06cd6e
Guillaume THOMAS <guillaumethomas@outlook.com>
2019-01-05 13:24:05 -0800

    PEP 8

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
+
+# Hello World from GPU
+
+if __name__ == "__main__":
+    hello = tf.constant('Hello, TensorFlow!')
+    sess = tf.Session()
+    print(sess.run(hello))
